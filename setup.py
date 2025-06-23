46d5b00a322fd54754ddc60ac03a020906f85d7f
phragman <a@staphi.com>
2025-06-23 13:55:54 -0700

    inc version 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="langfabric",
-    version="0.1.5",
+    version="0.1.6",
     description="Flexible LLM model initialization from YAML configuration.",
     author="Phragman",
     author_email="a@staphi.com",
